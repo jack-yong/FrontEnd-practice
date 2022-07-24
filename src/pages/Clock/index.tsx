93cc1ab2f6c0a4935c58761724e06d741b96356e
jack-yong <1807413446@qq.com>
2022-07-24 20:34:15 +0800

    初始化项目目录

---
+++ src/pages/Clock/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './index.module.scss';
+
+const Clock = (props: any) => {
+    return (
+        <div className={styles.clock}>Clock</div>
+    )
+}
+
+export default Clock
